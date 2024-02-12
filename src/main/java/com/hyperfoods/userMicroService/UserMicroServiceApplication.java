3328e23e865d13f85ac63b9255d0589b93252d34
Thiago Ribeiro <trcosta97@hotmail.com>
2024-02-12 11:27:36 -0300

    mvn files + main class

---
+++ src/main/java/com/hyperfoods/userMicroService/UserMicroServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hyperfoods.userMicroService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserMicroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserMicroServiceApplication.class, args);
+	}
+
+}
